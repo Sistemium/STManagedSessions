d4a8cd597a3122870dac0726691edc23899ba67c
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-05-07 15:58:14 +0400

    add trackers

---
+++ STManagedSessions/Classes/STLocationTracker.h
@@ -0,0 +1,18 @@
+//
+//  STGTLocationTracker.h
+//  geotracker
+//
+//  Created by Maxim Grigoriev on 4/3/13.
+//  Copyright (c) 2013 Maxim Grigoriev. All rights reserved.
+//
+
+#import "STTracker.h"
+
+@interface STLocationTracker : STTracker
+
+@property (nonatomic) CLLocationAccuracy currentAccuracy;
+@property (nonatomic, strong) CLLocation *lastLocation;
+
+- (void)getLocation;
+
+@end
---
+++ STManagedSessions/Classes/STTracker.h
@@ -0,0 +1,27 @@
+//
+//  STGTTracker.h
+//  geotracker
+//
+//  Created by Maxim Grigoriev on 3/11/13.
+//  Copyright (c) 2013 Maxim Grigoriev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "STSessionManagement.h"
+#import "STDocument.h"
+
+@interface STTracker : NSObject
+
+@property (nonatomic, strong) STDocument *document;
+@property (nonatomic, strong) id <STSession> session;
+@property (nonatomic, strong) NSMutableDictionary *settings;
+@property (nonatomic, strong) NSString *group;
+@property (nonatomic) BOOL tracking;
+@property (nonatomic) BOOL trackerAutoStart;
+
+- (void)customInit;
+- (void)startTracking;
+- (void)stopTracking;
+
+@end
